10234eecf21b0cbfb4a69ace6d9d2379aa155c51
Lem Lordje Ko <lord.lem.l@gmail.com>
2017-06-06 00:12:45 +0800

    add git configs

--- _all.bash
+++ _all.bash
@@ -4,3 +4,4 @@ source $DIR/aliases.bash
 source $DIR/completions.bash
 source $DIR/dock.bash
 source $DIR/exports.bash
+source $DIR/git.bash
---
+++ git.bash
@@ -0,0 +1,8 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.st status
+
+git config --global user.name "Lem Lordje Ko"
+git config --global user.email lord.lem.l@gmail.com
+
+git config --global push.default upstream
