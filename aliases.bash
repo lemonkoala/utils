8e57e61ac8f38222dfa9b403358da0fb75a38d93
Lem Lordje Ko <lord.lem.l@gmail.com>
2017-08-20 01:55:05 +0800

    add alias fucking=sudo

--- aliases.bash
+++ aliases.bash
@@ -2,3 +2,5 @@ alias ~='cd ~'
 alias la='ls -la'
 alias lh='ls -lh'
 alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
+
+alias fucking=sudo
