d68359956d6de0e27df435ffa672bc19389ec4bf
Lem Lordje Ko <ko-lem@protonmail.com>
2018-07-17 20:27:06 +0800

    add virtualenv support

--- exports.bash
+++ exports.bash
@@ -1,4 +1,4 @@
-export PS1="\e[35m\u@\h\e[m \e[33m\w\e[0m \$(parse_git_branch) \n\[\e[33m\]-->\[\e[m\] "
+export PS1="\e[35m\u@\h\e[m \e[33m\w\e[0m  \$(parse_git_branch) \$(parse_venv) \n\[\e[33m\]-->\[\e[m\] "
 export PS2="->"
 
 export CLICOLOR=1
@@ -10,6 +10,12 @@ export PATH="$PATH:$HOME/bin"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+export VIRTUAL_ENV_DISABLE_PROMPT=true
+
 parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+parse_venv() {
+  basename $VIRTUAL_ENV 2> /dev/null | sed -e 's/\(.*\)/[\1]/'
 }
