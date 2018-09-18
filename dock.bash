bf916c76c933391f0c168c31b30579eb5b361be3
Lem Lordje Ko <ko-lem@protonmail.com>
2018-09-18 22:02:21 +0800

    add pruning of unused networks on dockclean

--- dock.bash
+++ dock.bash
@@ -75,6 +75,7 @@ dockclean() {
   docker rm -v $(docker ps     --filter status=exited -q 2>/dev/null) 2>/dev/null
   docker rmi   $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
   docker volume rm $(docker volume ls -qf dangling=true) 2>/dev/null
+  docker network prune -f
 }
 
 alias dm='docker-machine'
